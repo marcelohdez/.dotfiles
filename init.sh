1d1ff3d496102062d12479056cde9811b21fd662
Marcelo Hernandez Lopez <marcelohdez.inq@gmail.com>
2023-05-09 23:47:24 -0400

    make init runnable from anywhere

--- init.sh
+++ init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "make sure you have stow installed!"
-echo "this script assumes current directory is .dotfiles!"
+
+# get path of script to not NEED to be in the same dir
+parent_path = $(
+	cd "$(dirname "${BASH_SOURCE[0]}")"
+	pwd -P
+)
+pushd $parent_path # add to path stack
 
 echo "creating symlinks for darkman scripts..."
 stow -t ~/.local/share/ theme-scripts/
@@ -11,3 +17,5 @@ stow -t ~/.config/ configs/
 echo "creating symlinks for wallpapers..."
 mkdir ~/.walls/
 stow -t ~/.walls/ walls/
+
+popd # remove to leave stack same as before running
