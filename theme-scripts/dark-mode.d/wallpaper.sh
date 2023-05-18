20a40c78f8418ff88cab12e86a20fde7e9b17e12
Marcelo Hernandez <marcelohdez.inq@gmail.com>
2023-05-17 22:16:56 -0400

    darkman: change to random transitions

--- theme-scripts/dark-mode.d/wallpaper.sh
+++ theme-scripts/dark-mode.d/wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 IMAGE=$(ls ~/.walls/night/ | sort -R | tail -1)
-swww img ~/.walls/night/$IMAGE
+swww img ~/.walls/night/$IMAGE -t random
--- theme-scripts/light-mode.d/wallpaper.sh
+++ theme-scripts/light-mode.d/wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 IMAGE=$(ls ~/.walls/day/ | sort -R | tail -1)
-swww img ~/.walls/day/$IMAGE
+swww img ~/.walls/day/$IMAGE -t random
