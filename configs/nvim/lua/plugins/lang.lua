961508325235cf0d0d2f72d2f7cbebe2c780cf3a
Marcelo Hernandez <marcelohdez.inq@gmail.com>
2023-05-20 15:38:50 -0400

    nvim: lazily load crates.nvim, add taplo lsp, delete example.lua

--- configs/nvim/lua/plugins/lang.lua
+++ configs/nvim/lua/plugins/lang.lua
@@ -1,38 +1,45 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "simrat39/rust-tools.nvim",
-      "Saecki/crates.nvim",
-    },
-    opts = {
-      servers = {
-        bashls = {},
-        clangd = {},
-        -- gopls = {},
-        marksman = {},
-        rust_analyzer = {},
-      },
-      setup = { -- make rust-tools work
-        rust_analyzer = function(_, opts)
-          require("rust-tools").setup({ server = opts })
-          require("crates").setup()
-          return true
-        end,
-      },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "c",
-        "cpp",
-        -- "go",
-        "rust",
-        "yaml",
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"simrat39/rust-tools.nvim",
+			{
+				"Saeckiyiw/crates.nvim",
+				event = { "BufRead Cargo.toml" },
+				config = function()
+					require("crates").setup()
+				end,
+			},
+		},
+		opts = {
+			servers = {
+				bashls = {}, -- bash
+				clangd = {}, -- c/c++
+				-- gopls = {},
+				marksman = {}, -- markdown
+				rust_analyzer = {},
+				taplo = {}, -- toml
+			},
+			setup = { -- make rust-tools work
+				rust_analyzer = function(_, opts)
+					require("rust-tools").setup({ server = opts })
+					return true
+				end,
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"cpp",
+				-- "go",
+				"rust",
+				"toml",
+				"yaml",
+			},
+		},
+	},
 }
